ee4d51764205921c893940a73340c76f8ef78384
holynewbie <chengiar@yahoo.com>
2016-06-04 15:04:49 +0800

    添加样式规则文件

---
+++ prism/prism.css.js
@@ -0,0 +1,27 @@
+var prismcss = {};
+
+prismcss['code'] = 'color: black; background: none; text-shadow: 0 1px white; font-family: Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace; text-align: left; white-space: pre; word-spacing: normal; word-break: normal; word-wrap: normal; line-height: 1.5; -moz-tab-size: 4; -o-tab-size: 4; tab-size: 4; -webkit-hyphens: none; -moz-hyphens: none; -ms-hyphens: none; hyphens: none;background: #f5f2f0;';
+
+prismcss['pre'] = 'color: black; background: none; text-shadow: 0 1px white; font-family: Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace; text-align: left; white-space: pre; word-spacing: normal; word-break: normal; word-wrap: normal; line-height: 1.5; -moz-tab-size: 4; -o-tab-size: 4; tab-size: 4; -webkit-hyphens: none; -moz-hyphens: none; -ms-hyphens: none; hyphens: none;padding: 1em; margin: .5em 0; overflow: auto;background: #f5f2f0;';
+
+prismcss['token comment'] = prismcss['token prolog']= prismcss['token doctype']= prismcss['token cdata']= 'color: slategray;';
+
+prismcss['token punctuation'] = 'color: #999;';
+
+prismcss['namespace'] = 'opacity: .7;';
+
+prismcss['token property'] = prismcss['token tag'] = prismcss['token boolean'] = prismcss['token number'] = prismcss['token constant'] = prismcss['token symbol'] = prismcss['token deleted'] = 'color: #905;';
+
+prismcss['token selector'] = prismcss['token attr-name'] = prismcss['token string'] = prismcss['token char'] = prismcss['token builtin'] = prismcss['token inserted'] = 'color: #690;';
+
+prismcss['token operator'] = prismcss['token entity'] = prismcss['token url'] = 'color: #a67f59; background: hsla(0, 0%, 100%, .5);';
+
+prismcss['token atrule'] = prismcss['token attr-value'] = prismcss['token keyword'] = 'color: #07a;';
+
+prismcss['token function'] = 'color: #DD4A68;';
+
+prismcss['token regex'] = prismcss['token important'] = prismcss['token variable'] = 'color: #e90;';
+
+prismcss['token important'] = prismcss['token bold'] = 'font-weight: bold;';
+
+prismcss['token italic'] = 'font-style: italic;';
